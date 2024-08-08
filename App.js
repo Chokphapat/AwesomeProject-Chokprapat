e0994338f2c16ee4459bf28e216fd126282c94c6
Chokprapat <151495158+Chokphapat@users.noreply.github.com>
2024-08-08 13:10:22 +0700

    week6

--- App.js
+++ App.js
@@ -15,11 +15,13 @@ import Ex10 from './screens/week2/Ex10';
 import Ex11 from './screens/week2/Ex11';
 import Ex12 from './screens/week2/Ex12';
 import Test from './screens/week3/test';
-import { View } from 'react-native';
+import { FlatList, View } from 'react-native';
 import Greeting from './components/Greeting';
 import Travel from './screens/week3/Travel';
 import Resort from './screens/week3/Resort';
 import Health from './screens/week5/Health';
+import Home from './screens/week6/Home';
+import FlatListExample from './screens/week6/FlatListExample';
 
 export default function App(){
     return (
@@ -44,6 +46,8 @@ export default function App(){
       // </View>
       // <Travel />
       // <Resort/>
-      <Health/>
+      // <Health/>
+      <Home/>
+      // <FlatListExample/>
     );
 }
